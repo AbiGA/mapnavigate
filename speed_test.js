7aa5834ec2e885d35ce2336845afa4c7807237e6
AbiGA <abhishekjoshi724@gmail.com>
2019-02-20 13:11:58 +0530

    initial commit

---
+++ speed_test.js
@@ -0,0 +1,133 @@
+function $(element) {
+  return document.getElementById(element);
+}
+
+var speedTest = {};
+
+speedTest.pics = null;
+speedTest.map = null;
+speedTest.markerClusterer = null;
+speedTest.markers = [];
+speedTest.infoWindow = null;
+
+speedTest.init = function() {
+  var latlng = new google.maps.LatLng(12.9279, 77.6271);
+  var options = {
+    'zoom': 4,
+    'center': latlng,
+    'mapTypeId': google.maps.MapTypeId.ROADMAP,
+	'gestureHandling': 'cooperative'
+  };
+
+  speedTest.map = new google.maps.Map($('map'), options);
+  speedTest.pics = data.photos;
+  
+  
+
+  speedTest.infoWindow = new google.maps.InfoWindow();
+
+  speedTest.showMarkers();
+};
+
+speedTest.showMarkers = function() {
+  speedTest.markers = [];
+
+  var type = 1;
+  
+
+  if (speedTest.markerClusterer) {
+    speedTest.markerClusterer.clearMarkers();
+  }
+
+  var panel = $('markerlist');
+  panel.innerHTML = '';
+  var numMarkers = 10;
+  for (var i = 0; i < numMarkers; i++) {
+    var titleText = speedTest.pics[i].photo_title;
+    if (titleText === '') {
+      titleText = 'No title';
+    }
+
+    var item = document.createElement('DIV');
+    var title = document.createElement('A');
+    title.href = '#';
+    title.className = 'title';
+    title.innerHTML = titleText;
+
+    item.appendChild(title);
+    panel.appendChild(item);
+
+
+    var latLng = new google.maps.LatLng(speedTest.pics[i].latitude,
+        speedTest.pics[i].longitude);
+
+    var imageUrl = 'http://chart.apis.google.com/chart?cht=mm&chs=24x32&chco=' +
+        'FFFFFF,008CFF,000000&ext=.png';
+    var markerImage = new google.maps.MarkerImage(imageUrl,
+        new google.maps.Size(24, 32));
+
+    var marker = new google.maps.Marker({
+      'position': latLng,
+      'icon': markerImage
+    });
+	
+    var fn = speedTest.markerClickFunction(speedTest.pics[i], latLng);
+    google.maps.event.addListener(marker, 'click', fn);
+    google.maps.event.addDomListener(title, 'click', fn);
+	if (speedTest.pics[i]['category']==='B') {
+		marker.setIcon('https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png');
+	}
+    speedTest.markers.push(marker);
+  }
+
+  window.setTimeout(speedTest.time, 0);
+};
+
+speedTest.markerClickFunction = function(pic, latlng) {
+  return function(e) {
+    e.cancelBubble = true;
+    e.returnValue = false;
+    if (e.stopPropagation) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+	  speedTest.map.setZoom(6);
+	    speedTest.map.setCenter(latlng);
+    var title = pic.photo_title;
+    var url = pic.photo_url;
+    var fileurl = pic.photo_file_url;
+
+    var infoHtml = '<div class="info"><h3>' + title +
+      '</h3><div class="info-body">' +
+      '<a href="' + url + '" target="_blank"><img src="' +
+      fileurl + '" class="info-img"/></a></div>' +
+      '<a href="http://www.panoramio.com/" target="_blank">' +
+      '<img src="http://maps.google.com/intl/en_ALL/mapfiles/' +
+      'iw_panoramio.png"/></a><br/>' +
+      '<a href="' + pic.owner_url + '" target="_blank">' + pic.owner_name +
+      '</a></div></div>';
+
+    speedTest.infoWindow.setContent(infoHtml);
+	
+    speedTest.infoWindow.setPosition(latlng);
+    speedTest.infoWindow.open(speedTest.map);
+  };
+};
+
+
+
+speedTest.change = function() {
+  speedTest.clear();
+  speedTest.showMarkers();
+};
+
+speedTest.time = function() {
+
+    var start = new Date();
+    for (var i = 0, marker; marker = speedTest.markers[i]; i++) {
+      marker.setMap(speedTest.map);
+    }
+	
+  var end = new Date();
+  $('timetaken').innerHTML = end - start;
+};
